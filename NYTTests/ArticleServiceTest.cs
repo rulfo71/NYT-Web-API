1a83f009863b2b0114e2e632382c0c2d571dc8a4
Tomas Pulenta - Hx <tpulenta@gmail.com>
2018-09-26 13:16:02 -0300

    parlaa

--- NYTTests/ArticleServiceTest.cs
+++ NYTTests/ArticleServiceTest.cs
@@ -18,13 +18,12 @@ namespace NYTTests
             string begin_date = "20160901";
             string end_date = "20160902";
             var articlesService = new ArticlesService();
-            Task<List<Doc>> articlesTask = articlesService.GetjsonAsync(theme,begin_date,end_date);
+            Task<List<Doc>> articlesTask = articlesService.GetNewsAsync(theme,begin_date,end_date);
             var articles = articlesTask.Result;
             var quantityOfArticles = articles.Count();
             var result = (quantityOfArticles == 10);
             Assert.IsTrue(result ,"There should be maximum 10 articles");
         }
-
     }
 }
 
